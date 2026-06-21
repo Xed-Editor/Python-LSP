884a680896e43b8227b018e0978d5c52a172b580
Rohit Kushwaha <oldisg131@gmail.com>
2026-06-21 11:37:18 +0530

    fix: diagnostics

--- app/src/main/java/com/rk/xededitor/python/PythonServer.kt
+++ app/src/main/java/com/rk/xededitor/python/PythonServer.kt
@@ -3,10 +3,19 @@ package com.rk.xededitor.python
 import android.app.Activity
 import android.content.Context
 import com.rk.file.BuiltinFileType
+import com.rk.file.FileObject
 import com.rk.icons.Icon
 import com.rk.lsp.LspConnectionConfig
 import com.rk.lsp.LspServer
+import io.github.rosemoe.sora.event.ContentChangeEvent
+import io.github.rosemoe.sora.event.Unsubscribe
+import io.github.rosemoe.sora.lsp.events.EventType
+import io.github.rosemoe.sora.lsp.events.diagnostics.queryDocumentDiagnostics
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.eclipse.lsp4j.DocumentDiagnosticReport
 import java.io.File
+import java.net.URI
 
 class PythonServer(val lspPath: String) : LspServer() {
     override val id: String = "rk.python"
@@ -35,4 +44,16 @@ class PythonServer(val lspPath: String) : LspServer() {
         File(lspPath).setExecutable(true)
         return LspConnectionConfig.Process(arrayOf(lspPath,"server"))
     }
+
+    override val canBeUninstalled: Boolean = false
+
+    override suspend fun onInitialize(lspConnector: com.rk.lsp.LspConnector) {
+        lspConnector.lspEditor?.requestManager?.didChangeConfiguration(
+            org.eclipse.lsp4j.DidChangeConfigurationParams(
+                mapOf("ty" to mapOf("diagnosticMode" to "workspace"))
+            )
+        )
+    }
+
+    override fun getInitializationOptions(uri: URI?): Any = mapOf("diagnosticMode" to "workspace")
 }
